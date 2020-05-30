eb828bf974829405f6ff8d6ad7afea8c42e4f558
Rambrus01 <rambrus0@gmail.com>
2020-05-30 11:12:34 +0200

    Add and Delete function added.

---
+++ PhoneBook/Model/PhoneNumberManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Model
+{
+    public class PhoneNumberManager
+    {
+        private List<PhoneNumber> phoneNumbers = new List<PhoneNumber>();
+
+        public PhoneNumberManager()
+        {
+        }
+
+        public void AddPhoneNumber(PhoneNumber phoneNumber)
+        {
+            phoneNumbers.Add(phoneNumber);
+        }
+
+        public void DeletePhoneNumber(PhoneNumber phoneNumber)
+        {
+            phoneNumbers.Remove(phoneNumber);
+        }
+    }
+}
