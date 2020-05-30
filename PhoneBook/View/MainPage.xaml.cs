1e08db4da2b12598d12c6ceb6d827fc6032e103b
Rambrus01 <rambrus0@gmail.com>
2020-05-30 11:00:10 +0200

    Project Scheme Folders created.

--- PhoneBook/View/MainPage.xaml.cs
+++ PhoneBook/View/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace PhoneBook
+namespace View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,6 +25,11 @@ namespace PhoneBook
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
         }
     }
 }
